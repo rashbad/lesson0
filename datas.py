ef8dedace837548754053a7b86599f8183f29511
rashbad <avtoritetli@vk.com>
2024-08-07 19:09:18 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,5 @@
+print('Hello, "world!"') # // - целочисленное деление, / - деление
+print(type ('Hello,world!'))   # sring - строка
+         # boolean                                   #concatenate
+
+print(type(bool ('5')))
