e1cf72236e3515844f941a82539823416c0be383
Rashad <avtoritetli@vk.com>
2024-08-18 18:15:04 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,6 +1,6 @@
 my_string = input('Город проживания: ')
 print(len(my_string))
-my_string = 'Как здорово, что все мы здесь - сегодня собрались'
+my_string = 'Город проживания: '
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(' ', ''))
